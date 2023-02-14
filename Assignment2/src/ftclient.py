94b24cd6587ffedf9507865f7f9195a9ab8e7654
MagnusDH <magnusp_96@hotmail.com>
2023-02-14 14:31:55 +0100

    ...

--- Assignment2/src/ftclient.py
+++ Assignment2/src/ftclient.py
@@ -1,63 +1,101 @@
 import socket
-import time
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
 
-client_public_key = "cl13nt pu8l1c k3y"
-client_private_key = "cl13nt pr1v4t3 k3y"
-server_public_key = ""
+key = RSA.generate(2048)
+client_private_key = key.exportKey() 
+client_public_key = key.publickey().exportKey()
+server_public_key = None
+session_key = get_random_bytes(16)
+print("\nSESSION KEY: ", session_key)
+
+def connect_to_server(host_name, port_number):
+    print("Sending connection request to server: ", host_name, "\n")
+
+    try:
+        client_socket.connect((host_name, port_number))
+        server_response = client_socket.recv(1024).decode()
+        
+        if(server_response.lower() == "request accepted"):
+            print("Server accepted connection request!\n")
+            print("CONNECTION ESTABLISHED\n")
+        else:
+            print("Server declined request...\n")
+   
+    except:
+        print("Could not connect to server...\n")
+        #Close connection
+        client_socket.close()
+
+def receive_public_key():
+    #Receive server_public_key
+    print("Waiting to receive server_public_key\n")
+    server_response = client_socket.recv(1024)
+    server_public_key = server_response
+
+def send_and_receive_data():
+    while True:
+        #User can write a message
+        message = input("Response to server: ")
+        #Send a message
+        client_socket.send(message.encode())
+        
+        #Close connection if requested
+        if (message.lower() == "close"):
+            print("Closing connection...")
+            client_socket.close()
+            break
+        
+        #Receive response from server
+        print("Waiting for server to respond...")
+        response_data = client_socket.recv(1024).decode()
+        print("Response from server: ", response_data, "\n")
+
+#START
 
 #Obtain the name of the client (magnus-linux)
 host_name = socket.gethostname()
-port_number = 5000
+port_number = 5001
 
 #Get socket instance
 client_socket = socket.socket()
 
-#Try to connect to a server
-print("Sending connection request to server: ", host_name, "\n")
-try:
-    client_socket.connect((host_name, port_number))
-    server_response = client_socket.recv(1024).decode()
-    if(server_response.lower() == "request accepted"):
-        print("Server accepted connection request!\n")
-        print("CONNECTION ESTABLISHED\n")
-    else:
-        print("Server declined request...\n")
-except:
-    print("Could not connect to server...\n")
-    #Close connection
-    client_socket.close()
+connect_to_server(host_name, port_number)
 
 #send client_public_key to server
 print("Sending public key...\n")
-message = client_public_key
-client_socket.send(message.encode())
+client_socket.send(client_public_key)
 
 #Receive server_public_key
-print("Waiting to receive server_public_key\n")
-server_response = client_socket.recv(1024).decode()
-print("Got server_public_key: ", server_response, "\n")
-server_public_key = server_response
-
-#Send and receive data
-while True:
-    #User can write a message
-    message = input("Response to server: ")
-    #Send a message
-    client_socket.send(message.encode())
-    
-    #Close connection if requested
-    if (message.lower() == "close"):
-        print("Closing connection...")
-        break
-    
-    #Receive response from server
-    print("Waiting for server to respond...")
-    response_data = client_socket.recv(1024).decode()
-    print("Response from server: ", response_data, "\n")
+print("\nReceiving server_public_key")
+server_public_key = client_socket.recv(2048)
 
 
-#Close connection
-client_socket.close()
+#Encrypt session key with server_public_key and send
+cipher_rsa = PKCS1_OAEP.new(server_public_key)
+encrypted_session_key = cipher_rsa.encrypt(session_key)
+client_socket.send(encrypted_session_key)
+print("ENCRYPTED SESSION KEY:   ", encrypted_session_key)
+
+# #receive file via AES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Assignment2/src/ftserv.py
+++ Assignment2/src/ftserv.py
@@ -1,65 +1,130 @@
+#pip install pycryptodome
 import socket
-import time
-from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+key = RSA.generate(2048)
+server_private_key = key.exportKey() 
+server_public_key = key.publickey().exportKey()
+client_public_key = None
+session_key = None
 
 
-server_public_key = "53rv3r Pu8l1c k3y"
-server_private_key = "53rv3r pr1v4t3 k3y"
-client_public_key = ""
+def create_socket(port_number):
+    """
+    -Create socket instance\n
+    -Obtain name of host\n
+    -bind host and given number"""
+    server_host_name = socket.gethostname()
 
-#Obtain the name of the host (magnus-linux)
-server_host_name = socket.gethostname()
-port_number = 5000
+    server_socket = socket.socket()
 
-#Get socket instance
-server_socket = socket.socket()
+    server_socket.bind((server_host_name, port_number))
 
-#Bind host address and port numer together
-server_socket.bind((server_host_name, port_number))
+    return server_socket
 
-#Listen for 5 connections
-print("Waiting for clients to connect...\n")
+def connect_to_client(): 
+    """
+   -Accept an incomming connection\n
+   -Send accept message back to client"""
+    try:
+        connection, address = server_socket.accept()
+        print("Got connection request. Establishing connection with: ", address, "\n")
+        message = "request accepted"
+        connection.send(message.encode())
+        print("CONNECTION ESTABLISHED!\n")
+        return connection, address
+
+    except:
+        print("Could not establish connection with client...")
+
+def receive_and_send_data():
+
+    while True:
+        #Receive data
+        print("Waiting for client to respond...\n")
+        receive_data = connection.recv(1024).decode()
+        print("Response from client: ", receive_data, "\n")
+        
+        if receive_data.lower() == "close":
+            print("Closing connection...")
+            connection.close()
+
+        else:
+            #Send data from server
+            response = input("Response to client: ")
+            connection.send(response.encode())
+    
+    
+#START
+
+#Create socket
+server_socket = create_socket(5001)
+
+#Listen for connections
+print("\nWaiting for clients to connect...")
 server_socket.listen(5)
 
-#When connection request is recieved, connect to client
-try:
-    connection, address = server_socket.accept()
-    print("Got connection request. Establishing connection with: ", address, "\n")
-    message = "request accepted"
-    connection.send(message.encode())
-    print("CONNECTION ESTABLISHED!\n")
-except:
-    print("Could not establish connection with client...")
+#Connect to client
+connection, address = connect_to_client()
 
 #Receive client_public_key
-print("Waiting to receive public key...\n")
-client_response = connection.recv(1024).decode()
-print("Got client pulic key: ", client_response, "\n")
-client_public_key = client_response
+print("\nReceiving client public key...")
+client_public_key = connection.recv(1024)
 
 #send server_public_key
-print("Sending server_public_key...\n")
-message = server_public_key
-connection.send(message.encode())
+print("\nSending server_public_key...")
+connection.send(server_public_key)
 
-#Receive and send data
-while True:
-    #Receive data
-    print("Waiting for client to respond...\n")
-    receive_data = connection.recv(1024).decode()
-    print("Response from client: ", receive_data, "\n")
-    
-    if receive_data.lower() == "close":
-        print("Closing connection...")
-        break
+#receive and decrypt session key
+print("TEST MAFAKKA")
 
-    else:
-        #Send data from server
-        response = input("Response to client: ")
-        connection.send(response.encode())
+encrypted_session_key = connection.recv(2048)
+print("ENCRYPTED SESSION KEY: ", encrypted_session_key)
 
-#Close the connection
-connection.close()
+# cipher_rsa = PKCS1_OAEP.new(server_private_key)
+# session_key = cipher_rsa.decrypt(encrypted_session_key)
+# print("\nSESSION KEY: ", session_key)
+
+
+
+
+
+
+
+        #FLOW OF PROGRAM
+#client -> server: connection request
+#client <- server: accept request
+#Client -> server: send public key
+#client <- server: send public key
+#client -> server: session key
+#client <_ server: file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
